5c55b8f315df1904bdf38c33dd569f413ac59a9f
Yi-Ying He <b00502040@ntu.edu.tw>
2015-06-26 05:14:33 +0800

    output train data

--- training_v2.py
+++ training_v2.py
@@ -18,72 +18,70 @@ def get_category_im(c,method):
 
 
 def getFile(filedir):
-    fileData = []
+    fileData = [[]]
+    for file_idx in xrange(200):
+        fileData.append([])
+        for file in glob.glob(filedir + '*_' + str(file_idx + 1) +'.txt'):
+            with open(file, 'r') as f:
+                line_num = 0
+                for line in f:
+                    fileData[file_idx + 1].append(line.strip().split())
+                    fileData[file_idx + 1][line_num][0] = ""
+                    line_num += 1
     return fileData
 
+def printFile(filedir, fileData):
+    for file_idx in xrange(200):
+        outFile = open(filedir + '/' + str(file_idx + 1) + '.txt', 'w')
+        print 'write %d' % (file_idx + 1)
+        for sentence in fileData[file_idx + 1]:
+            if sentence[0] == '':
+                outFile.write(str(8) + ' ')
+            else:
+                outFile.write(sentence[0] + ' ')
+            for word in sentence[1:]:
+                outFile.write(word)
+            outFile.write('\n')
+        outFile.close
+    
 
-def test(category, method):
-    param = {[0, 17, 10, 24, 29, 22, 13, 7]}
+def test(category, method, fileData):
+    param = {'word_importance' : [0, 17, 10, 22, 21, 36, 11, 6],
+             'cross_entropy' : [0, 17, 10, 24, 29, 22, 13, 7]}
     im_word,im_weight = get_category_im(category, method)
     threshold = param[method][category]
-    dir = 'training/training_merge/'
-    real_ans = 0
-    retrieve = 0
-    right_predict = 0
-
-    if not os.path.exists(err_dir):
-        os.mkdir(err_dir)
+    predict = [[]]
 
     for file_idx in xrange(200):
-        for file in glob.glob(dir + '*_' + str(file_idx + 1) +'.txt'):
-            path, input = os.path.split(file)
-            idx = int(input.split('_')[1].split('.')[0])
-            if idx %10 == 0:
-                continue
-            with open(file, 'r') as f:
-                print file
-                lineNum = 0
-                for line in f:
-                    lineNum += 1
-                    token = line.strip().split()
-                    is_ans = False
-                    is_retrieve = False
-                    if str(category) in token[0]:
-                        real_ans += 1
-                        is_ans = True
-                    for word in token[1:]:
-                        if word in im_word and im_word.index(word) < threshold:
-                            retrieve += 1
-                            if is_ans:
-                                right_predict += 1
-                                #print 'right'
-                            #else:
-                                #g.write('%s: Line %d\n' %(input,lineNum))
-                                #g.write(line)
-                                #g.write('program error prediction: %d\n' %category)
-                                #g.write('\n')
-                                #print 'error'
-                            is_retrieve = True
-                            break
-                    #if not is_retrieve and is_ans:
-                        #g.write('%s: Line %d\n' %(input,lineNum))
-                        #g.write(line)
-                        #g.write('program did not classify it as category %d\n' %category)
-                        #g.write('\n')
-         
-    
+        print "file id %d, category %d" % (file_idx + 1, category)
+        predict.append([])
+        line_num = 0
+        for line in fileData[file_idx + 1]:
+            for word in line[1:]:
+                if word in im_word and im_word.index(word) < threshold:
+                    predict[file_idx + 1].append(line_num)
+                    break
+            line_num += 1
+    return predict
+
+
 if __name__ == '__main__':
     print 'run ngram...'
-    inputdir = 'training/training_merge'
-    outputdir = 'training/training_train'
+    inputdir = 'training/training_merge/'
+    outputdir = 'ngram/train/'
+    webdir = 'training/training_train/'
     ngram_count(inputdir, outputdir, train=True)
     total_ngram,category_ngram_list,category_total_count_list = get_count_statistics()
     word_importance(total_ngram,category_ngram_list,category_total_count_list)
     cross_entropy(total_ngram,category_ngram_list,category_total_count_list)
+    fileData = getFile(inputdir)
     for c in xrange(1, 8):
         for m in ['cross_entropy']:
-            if not os.path.exists(m):
-                os.mkdir(m)  
-            test(c, m)
-
-
+            pred = test(c, m, fileData)
+            for i in xrange(1, 201):
+                for j in pred[i]:
+                    if fileData[i][j][0] == '':
+                        fileData[i][j][0] = str(c)
+                    else:
+                        fileData[i][j][0] += ',' + str(c)
+    printFile(webdir, fileData)
